6f666cd28e2faa95b4b1d0cd35d1c2af4bce8f52
D.J. Blume <dj.blume@fivetooldev.com>
2024-12-23 14:44:53 -0600

    Log dependencies' latest versions

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 
 use cargo_metadata::MetadataCommand;
 use reqwest;
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
 #[derive(Deserialize)]
 struct CrateInfo {
@@ -12,6 +12,13 @@ struct CrateInfo {
 #[derive(Deserialize)]
 struct CrateDetails {
     max_version: String,
+}
+
+#[derive(Serialize)]
+struct DependencyReport {
+    name: String,
+    current_version: String,
+    latest_version: String,
 }
 
 pub fn detect_file_type(path: &str) -> Option<&'static str> {
@@ -28,30 +35,35 @@ pub async fn parse_cargo_toml(path: &str) {
         .exec()
         .expect("Failed to parse Cargo.toml");
 
-    for package in metadata.packages {
-        println!("Parsing package: {}", &package.name);
-        fetch_latest_version(&package.name).await;
-    }
+    let first_package = metadata.packages.first().unwrap();
+    fetch_latest_version(&first_package.name).await;
 }
 
 pub async fn fetch_latest_version(crate_name: &str) {
     let url = format!("https://crates.io/api/v1/crates/{}", crate_name);
-    let response = reqwest::get(&url)
+    let client = reqwest::Client::new();
+    let response:CrateInfo = client.get(&url)
+        .header("User-Agent", "dep_tracker/1.0 (https://github.com/dj-blume9/DepTrail.git)")
+        .send()
         .await
         .expect("Failed to fetch data")
-        .text()
+        .json()
         .await
         .expect("Failed to parse JSON");
     
-    println!("Raw JSON data from {}: {}", &crate_name, response);
+       if let Some(crate_details) = response.crate_details {
+        println!(
+            "Latest version of {}: {}",
+            crate_name, crate_details.max_version
+        );
+    } else {
+        println!("No data available for crate: {}", crate_name);
+    }
+}
 
-   // if let Some(crate_details) = response.crate_details {
-     //   println!(
-       //     "Latest version of {}: {}",
-         //   crate_name, crate_details.max_version
-       // );
-  //  } else {
-  //      println!("No data available for crate: {}", crate_name);
-  //  }
+
+pub fn generate_report(report: Vec<DependencyReport>) {
+    let json = serde_json::to_string_pretty(&report).unwrap();
+    println!("{}", json);
 }
 
